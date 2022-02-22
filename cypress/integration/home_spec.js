fdadf84088a890268a1dae4d7f1bb948589a366b
Matthias Guenther <matthias@wikimatze.de>
2022-02-22 12:41:55 +0100

    Finish homespecs

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,46 +1,46 @@
-describe('My First Test', () => {
-  it('Visit the homepage', () => {
+describe('Homepage Tests', () => {
+  beforeEach(() => {
     cy.visit('http://padrinorb.com')
+  })
+
+  it('Visit the homepage', () => {
 
     cy.contains('View on GitHub')
   })
 
   it('Opens that chat windows', () => {
-    cy.visit('http://padrinorb.com')
     cy.contains('Open Chat').click()
   })
 
   it("Opens link to '/guides'", () => {
-    cy.visit('http://padrinorb.com')
     cy.contains('Guides').click()
 
     cy.url().should('include', '/guides')
   })
 
   it("Opens link to '/blog'", () => {
-    cy.visit('http://padrinorb.com')
     cy.contains('Blog').click()
 
     cy.url().should('include', '/blog')
   })
 
-  it("Open external API documentation", () => {
-    cy.visit('http://padrinorb.com')
+  it("Has link to external API documentation", () => {
     cy.contains('API').should('have.attr', 'href', 'http://www.rubydoc.info/github/padrino/padrino-framework')
   })
 
   it("Opens link to '/contribute'", () => {
-    cy.visit('http://padrinorb.com')
     cy.contains('Contribute').click()
 
     cy.url().should('include', '/contribute')
   })
 
   it("Opens link to '/contribute'", () => {
-    cy.visit('http://padrinorb.com')
     cy.contains('Contribute').click()
 
     cy.url().should('include', '/contribute')
   })
 
+  it("Has link to GitHub project URL", () => {
+    cy.contains('GitHub').should('have.attr', 'href', 'https://github.com/padrino/padrino-framework')
+  })
 })
